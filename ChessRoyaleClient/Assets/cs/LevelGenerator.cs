60f6a6a183005a614c07f94650be6396c69e2d26
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2018-08-11 19:11:44 -0700

    Start some unity rendering graphics things

---
+++ ChessRoyaleClient/Assets/cs/LevelGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour {
+
+    public Transform tile;
+
+	// Use this for initialization
+	void Start () {
+
+        for (int y = 0; y < 10; y ++) {
+            for (int x = 0; x < 10; x ++) {
+                Instantiate(tile, x * Vector3.right + y * Vector3.forward, Quaternion.identity);
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
